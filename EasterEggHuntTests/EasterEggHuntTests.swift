2615d32117e2d684d73137e4d1104c755b5e112b
Ari Carmo <arilson.simplicio@g.globo>
2025-06-03 11:51:48 -0300

    Initial Commit

---
+++ EasterEggHuntTests/EasterEggHuntTests.swift
@@ -0,0 +1,17 @@
+//
+//  EasterEggHuntTests.swift
+//  EasterEggHuntTests
+//
+//  Created by Arilson Simplicio on 03/06/25.
+//
+
+import Testing
+@testable import EasterEggHunt
+
+struct EasterEggHuntTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
